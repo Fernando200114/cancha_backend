52dbb15be648ad0ce4f85851b7073de454d787e5
Fernando200114 <fernandojosellullunacuichan@gmail.com>
2025-07-12 20:17:49 -0500

    deleted deply

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as crypto from 'crypto';
 
+if (typeof globalThis.crypto === 'undefined') {
+  globalThis.crypto = crypto as unknown as Crypto;
+}
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3013);
